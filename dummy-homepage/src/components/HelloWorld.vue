c7664927497912561ba775d4f7f2e8ffc454f955
push23 <kolosov4dev@gmail.com>
2022-08-10 02:37:24 +0400

    Update HelloWorld.vue

--- dummy-homepage/src/components/HelloWorld.vue
+++ dummy-homepage/src/components/HelloWorld.vue
@@ -273,13 +273,13 @@ margin-top: 10px;
         }
 
 
- @media screen and (max-width: 700px) {
+ @media screen and (max-width: 520px) {
 
   .topics {
     display: block;
-    min-width: 300px;
-    height: 318px;
-    margin: 29px auto 10px -12%;
+    min-width: 180px;
+    height: 555px;
+    margin: 29px auto auto auto;
 }
 
  }
